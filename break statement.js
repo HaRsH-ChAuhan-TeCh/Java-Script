d2656a6ddd8c6ddf671456b7615a1297a71f00fa
HaRsH-ChAuhan-TeCh <hiteshchauhan3200@gmail.com>
2024-10-15 08:30:15 +0530

    Add files via upload

---
+++ break statement.js
@@ -0,0 +1,21 @@
+<html>
+2. <body>
+3. <script type = "text/javascript">
+4. <!--
+5. var x = 1;
+6. document.write("Entering the loop<br /> ");
+7. while (x < 20)
+8. {
+a. if (x == 5)
+b. {
+c. break; // breaks out of loop completely
+d. }
+9. x = x + 1;
+10. document.write( x + "<br />");
+11. }
+12. document.write("Exiting the loop!<br /> ");
+13. //-->
+14. </script>
+15. <p>Set the variable to different value and then try...</p>
+16. </body>
+17. </html>
